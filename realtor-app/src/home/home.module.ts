bcd26acf99d2b645979f93461a2ab0c6f8082d99
Axel Wulff Sæther <axel.sather@soprasteria.com>
2022-09-01 12:48:19 +0200

    Etter Section 11: Implementing Authorization

--- realtor-app/src/home/home.module.ts
+++ realtor-app/src/home/home.module.ts
@@ -1,18 +1,13 @@
-import { ClassSerializerInterceptor, Module } from '@nestjs/common';
-import { APP_INTERCEPTOR } from '@nestjs/core';
-import { PrismaModule } from 'src/prisma/prisma.module';
-import { HomeController } from './home.controller';
-import { HomeService } from './home.service';
+import { ClassSerializerInterceptor, Module } from "@nestjs/common";
+import { APP_INTERCEPTOR } from "@nestjs/core";
+import { PrismaModule } from "src/prisma/prisma.module";
+import { HomeController } from "./home.controller";
+import { HomeService } from "./home.service";
 
 @Module({
-    imports: [PrismaModule],
-    controllers: [HomeController],
-    providers: [
-        HomeService,
-        {
-            provide: APP_INTERCEPTOR,
-            useClass: ClassSerializerInterceptor,
-        },
-    ],
+  imports: [PrismaModule],
+  controllers: [HomeController],
+  providers: [HomeService]
 })
-export class HomeModule {}
+export class HomeModule {
+}
